aa245ba4ce9831df25f43612cbe8f2f33e3bae10
ficky febrian <iky.febrian@gmail.com>
2024-09-14 21:28:24 +0700

    add menu: hot menu

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from "react";
 import MenuHeader from "../components/menu/menu";
 
 export default function Menu() {
+  const [hotMenu, setHotMenu] = useState([]);
   const [coldMenu, setColdMenu] = useState([]); // State untuk menyimpan data cold menu
+
+  useEffect(() => {
+    // Fetch the JSON file from public directory
+    fetch("/menu/hot.json")
+      .then(response => response.json())
+      .then(data => setHotMenu(data.items)) // Assuming the JSON format has "items"
+      .catch(error => console.error("Error fetching menu:", error));
+  }, []);
 
   useEffect(() => {
     // Fetch the JSON file from public directory
@@ -29,6 +38,26 @@ export default function Menu() {
             {coldMenu.length > 0 ? (
               <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
                 {coldMenu.map((item, index) => (
+                  <div key={index} className="flex justify-between items-start">
+                    <div>
+                      <h2 className="text-3xl font-playfair">{item.name}</h2>
+                      <p className="text-lg font-poppins text-gray-400">{item.description}</p>
+                    </div>
+                    <span className="text-xl font-poppins">{item.price}</span>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p className="text-center text-lg">Loading menu...</p>
+            )}
+          </div>
+        </div>
+        <div id="hot" className="flex justify-center text-5xl font-playfair px-24 py-24">Hot</div>
+        <div className="flex justify-center">
+          <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8">
+            {hotMenu.length > 0 ? (
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
+                {hotMenu.map((item, index) => (
                   <div key={index} className="flex justify-between items-start">
                     <div>
                       <h2 className="text-3xl font-playfair">{item.name}</h2>
